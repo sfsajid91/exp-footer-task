6706902a005a9372975924f0c7f7a99ef9a9242f
sfsajid91 <sfsajid91@gmail.com>
2025-05-04 23:01:58 +0600

    fix: ensure props are correctly spread onto the SVG element in ListIcon

--- src/components/icons/list.tsx
+++ src/components/icons/list.tsx
@@ -12,6 +12,7 @@ export default function ListIcon({
       viewBox="0 0 15 15"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      {...props}
     >
       <rect
         x="0.458984"
